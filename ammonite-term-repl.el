bb23cb31a8690000efd963bf3405f8066f02e5e6
zwild <zwildfree@gmail.com>
2018-12-29 14:58:23 +0800

    first commit

---
+++ ammonite-term-repl.el
@@ -0,0 +1,160 @@
+;;; ammonite-term-repl.el --- Scala Ammonite REPL in Emacs term mode.
+
+;; Copyright (C) 2018-2019 Wei Zhao
+
+;; Author: zwild <judezhao@outlook.com>
+;; Created: 2018-12-26T22:41:19+08:00
+;; URL: https://github.com/zwild/ammonite-term-repl
+;; Package-Requires: ((s "1.12.0") (scala-mode "0.23"))
+;; Version: 0.1
+;; Keywords: ammnite, term, scala
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(require 'term)
+(require 'comint)
+(require 'scala-mode)
+(require 's)
+
+(defgroup ammonite-term-repl nil
+  "A minor mode for a Ammonite REPL."
+  :group 'scala)
+
+(defcustom ammonite-term-repl-buffer-name "*Ammonite*"
+  "Buffer name for ammonite."
+  :type 'string
+  :group 'ammonite-term-repl)
+
+(defcustom ammonite-term-repl-program "amm"
+  "Program name for ammonite."
+  :type 'string
+  :group 'ammonite-term-repl)
+
+(defcustom ammonite-term-repl-program-args '()
+  "Arguments for ammonite command."
+  :type '(repeat string)
+  :group 'ammonite-term-repl)
+
+(defcustom ammonite-term-repl-prompt-regex "^@ "
+  "Regex for ammonite prompt."
+  :type 'string
+  :group 'ammonite-term-repl)
+
+(defcustom ammonite-term-repl-minor-mode-hook nil
+  "Hook to run after `ammonite-term-repl-minor-mode' is active."
+  :type 'hook
+  :group 'ammonite-term-repl)
+
+(defcustom ammonite-term-repl-run-hook nil
+  "Hook to run after starting an Ammonite REPL buffer."
+  :type 'hook
+  :group 'ammonite-term-repl)
+
+(defun ammonite-term-repl-check-process ()
+  (unless (comint-check-proc ammonite-term-repl-buffer-name)
+    (error "Ammonite is not running.")))
+
+(defun ammonite-term-repl-code-first-line (code)
+  (s-trim (car-safe (s-split "\n" code))))
+
+;;;###autoload
+(defun ammonite-term-repl-send-defun ()
+  (interactive)
+  (ammonite-term-repl-check-process)
+  (save-mark-and-excursion
+    (let (start end)
+      (scala-syntax:beginning-of-definition)
+      (setq start (point))
+      (scala-syntax:end-of-definition)
+      (setq end (point))
+      (let ((code (buffer-substring-no-properties start end)))
+        (comint-send-string ammonite-term-repl-buffer-name code)
+        (comint-send-string ammonite-term-repl-buffer-name "\n")
+        (message
+         (format "Sent: %s..." (ammonite-term-repl-code-first-line code)))))))
+
+;;;###autoload
+(defun ammonite-term-repl-send-region (start end)
+  "Send the region to the ammonite buffer."
+  (interactive "r")
+  (ammonite-term-repl-check-process)
+  (let ((code (buffer-substring-no-properties start end)))
+    (comint-send-string ammonite-term-repl-buffer-name "{\n")
+    (comint-send-string ammonite-term-repl-buffer-name code)
+    (comint-send-string ammonite-term-repl-buffer-name "\n}")
+    (comint-send-string ammonite-term-repl-buffer-name "\n")
+    (message
+     (format "Sent: %s..." (ammonite-term-repl-code-first-line code)))))
+
+;;;###autoload
+(defun ammonite-term-repl-send-buffer ()
+  "Send the buffer to the ammonite buffer."
+  (interactive)
+  (save-mark-and-excursion
+    (goto-char (point-min))
+    (re-search-forward "^package .+\n+" nil t)
+    (ammonite-term-repl-send-region (point) (point-max))))
+
+;;;###autoload
+(defun ammonite-term-repl-load-file (file-name)
+  (interactive (comint-get-source "Load Scala file: " nil '(scala-mode) t))
+  (comint-check-source file-name)
+  (with-temp-buffer
+    (insert-file-contents file-name)
+    (ammonite-term-repl-send-buffer)))
+
+;;;###autoload
+(defun ammonite-term-repl ()
+  "Run an Ammonite REPL."
+  (interactive)
+  (with-current-buffer
+      (apply 'term-ansi-make-term
+             ammonite-term-repl-buffer-name
+             ammonite-term-repl-program nil ammonite-term-repl-program-args)
+    (term-char-mode)
+    (term-set-escape-char ?\C-x)
+    (setq-local term-prompt-regexp ammonite-term-repl-prompt-regex)
+    (setq-local term-scroll-show-maximum-output t)
+    (setq-local term-scroll-to-bottom-on-output t)
+    (run-hooks 'ammonite-term-repl-run-hook))
+  (pop-to-buffer ammonite-term-repl-buffer-name))
+
+;;;###autoload
+(defalias 'run-ammonite 'ammonite-term-repl)
+
+(defvar ammonite-term-repl-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-`") 'ammonite-term-repl)
+    (define-key map (kbd "C-c C-z") 'ammonite-term-repl)
+    (define-key map (kbd "C-c C-e") 'ammonite-term-repl-send-defun)
+    (define-key map (kbd "C-c C-r") 'ammonite-term-repl-send-region)
+    (define-key map (kbd "C-c C-b") 'ammonite-term-repl-send-buffer)
+    (define-key map (kbd "C-c C-l") 'ammonite-term-repl-load-file)
+    map)
+  "Keymap while ammonite-term-repl-minor-mode is active.")
+
+;;;###autoload
+(define-minor-mode ammonite-term-repl-minor-mode
+  "Minor mode for interacting with an Ammonite REPL."
+  :keymap ammonite-term-repl-minor-mode-map
+  (run-hooks 'ammonite-term-repl-minor-mode-hook))
+
+(provide 'ammonite-term-repl)
+
+;;; ammonite-term-repl.el ends here
